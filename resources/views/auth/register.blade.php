1ae9ca8fe931a40fb5502858f82dfba44090efe6
Chan <chanamasawa@gmail.com>
2023-05-18 08:44:33 +0630

    retry validation ui

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,5 +6,19 @@
 
 @section('content')
     <h4>Register</h4>
-@endsection
+    <form action="{{ route('auth.store') }}" method="post">
+        @csrf
 
+        <div class=" mb-3">
+            <label for="title">Title</label>
+            <input id="title" type="text" value="{{ old('title') }}"
+                class="form-control
+            @error('title') is-invalid @enderror" name="title">
+            @error('title')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+    </form>
+@endsection
